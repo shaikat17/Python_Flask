c80aec5473f74907e7a6fe86b1741a9c06d88798
shaikat17 <shaikatpal56@gmail.com>
2022-12-05 00:25:54 +0600

    Registration and Login page is working

--- flask_blog_project/forms.py
+++ flask_blog_project/forms.py
@@ -1,25 +1,25 @@
-from wtforms import Form, BooleanField, SubmitField, PasswordField, StringField, validators
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, SubmitField, PasswordField, StringField, validators
 
 
-class RegistrationForm(Form):
+class RegistrationForm(FlaskForm):
     username = StringField(
         'Username', [validators.DataRequired(), validators.Length(min=4, max=25)])
     email = StringField(
-        'Email Address', [validators.Length(min=6, max=35), validators.Email()])
+        'Email Address', [validators.DataRequired(), validators.Email()])
     password = PasswordField('New Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords must match')
+        validators.DataRequired()
     ])
     confirm_password = PasswordField('Repeat Password', [
-        validators.DataRequired()])
+        validators.DataRequired(),
+        validators.EqualTo('password', message='Passwords must match')])
     submit = SubmitField('Sign Up')
 
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     email = StringField(
         'Email Address', [validators.Length(min=6, max=35), validators.Email()])
     password = PasswordField('New Password', [
         validators.DataRequired()])
-    remember = BooleanField('Remember Password', [
-        validators.DataRequired()])
+    remember = BooleanField('Remember Password')
     submit = SubmitField('Login')
--- flask_blog_project/main.py
+++ flask_blog_project/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -21,17 +21,29 @@ posts = [
 ]
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template('home.html', posts=posts)
 
 @app.route("/register", methods=['GET','POST'])
 def register():
     form = RegistrationForm()
+    print('hello before')
+    if form.validate_on_submit():
+        print('hello')
+        flash(f'Account Created for {form.username.data}', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Registration', form=form)
 
-@app.route("/login")
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 @app.route("/about")
