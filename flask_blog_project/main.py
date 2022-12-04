3c05a7538095d7c039d8e1ecd50ece70b3080162
shaikat17 <shaikatpal56@gmail.com>
2022-12-04 23:16:09 +0600

    Flask Blog Project Problem Solved

---
+++ flask_blog_project/main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '4736823462990396067dc3e604073207'
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route("/")
+def hello_world():
+    return render_template('home.html', posts=posts)
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Registration', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About Us')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
