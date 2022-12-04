4ac19de42cd9ee89ab6dcb01947fb8f823c6c762
shaikat17 <shaikatpal56@gmail.com>
2022-12-04 11:42:50 +0600

    Flask Blog Project

---
+++ Flask_Blog/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route("/")
+def hello_world():
+    return render_template('home.html', posts=posts)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
