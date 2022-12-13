93c3a1bed212c07a5c2bf2c15d098bb14f05d6ca
shaikat17 <shaikatpal56@gmail.com>
2022-12-13 23:45:22 +0600

    Profile Picture Update and Profile Information Update Added

--- flask_blog_project/Flask_Blog/forms.py
+++ flask_blog_project/Flask_Blog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import BooleanField, SubmitField, PasswordField, StringField, validators
 from Flask_Blog.models import User
 
@@ -33,3 +35,26 @@ class LoginForm(FlaskForm):
         validators.DataRequired()])
     remember = BooleanField('Remember Password')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField(
+        'Username', [validators.DataRequired(), validators.Length(min=4, max=25)])
+    email = StringField(
+        'Email Address', [validators.DataRequired(), validators.Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:    
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise validators.ValidationError(
+                    'That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise validators.ValidationError(
+                    'That email is taken. Please choose a different one.')
