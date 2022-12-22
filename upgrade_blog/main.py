824b0674af174b0f4541e8530416eb4a8e6d87d0
shaikat17 <shaikatpal56@gmail.com>
2022-12-23 00:24:23 +0600

    Simple blog project using flask completed

--- upgrade_blog/main.py
+++ upgrade_blog/main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+posts = requests.get('https://api.npoint.io/217117266f430a3e22db').json()
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', all_post = posts)
 
 @app.route("/about")
 def about():
@@ -14,6 +16,16 @@ def about():
 @app.route("/")
 def contact():
     return render_template('contact.html')
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
